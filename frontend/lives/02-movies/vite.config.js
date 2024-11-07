f0f07297806377d549675aed30d886925daed8e6
João Carvalho <oi@joaoc.dev>
2024-11-07 01:49:50 -0300

    teste: modifica diretório de build do vite

--- frontend/lives/02-movies/vite.config.js
+++ frontend/lives/02-movies/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lives/02-movies/',
+  build: {
+    outDir: '../../../front-end/lives/02-movies'
+  }
 })
